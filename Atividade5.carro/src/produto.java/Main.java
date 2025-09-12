29410925a299c95d7a3ff48cec4a9cfdde7b1925
persaudxz <heronpersaud123@gmail.com>
2025-09-11 22:23:11 -0300

    Add files via upload

---
+++ Atividade5.carro/src/produto.java/Main.java
@@ -0,0 +1,45 @@
+public class Produto {
+    private String nome;
+    private double preco;
+    private int quantidadeEstoque;
+
+    public Produto(String nome, double preco, int quantidadeEstoque) {
+        this.nome = nome;
+        setPreco(preco);
+        setQuantidadeEstoque(quantidadeEstoque);
+    }
+
+    // Getters e Setters
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        if (preco >= 0) {
+            this.preco = preco;
+        } else {
+            System.out.println("Preço não pode ser negativo. Valor não alterado.");
+        }
+    }
+
+    public int getQuantidadeEstoque() {
+        return quantidadeEstoque;
+    }
+
+    public void setQuantidadeEstoque(int quantidadeEstoque) {
+        if (quantidadeEstoque >= 0) {
+            this.quantidadeEstoque = quantidadeEstoque;
+        } else {
+            System.out.println("Quantidade em estoque não pode ser negativa. Valor não alterado.");
+        }
+    }
+}
+
