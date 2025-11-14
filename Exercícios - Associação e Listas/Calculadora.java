64323f5ee8437d8651093bcab6c34f805f3bfc73
persaudxz <heronpersaud123@gmail.com>
2025-11-13 22:39:02 -0300

    Add files via upload

---
+++ Exercícios - Associação e Listas/Calculadora.java
@@ -0,0 +1,10 @@
+public class Calculadora {
+
+    public double somar(double a, double b) {
+        return a + b;
+    }
+
+    public double multiplicar(double a, double b) {
+        return a * b;
+    }
+}
